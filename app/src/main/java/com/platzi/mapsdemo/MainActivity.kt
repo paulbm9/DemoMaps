69a4b77f5aa0638ecb4253c02baee4a08b875a21
m9 <paulbravomacedo@gmail.com>
2022-10-11 21:36:39 -0500

    Initial Commit

---
+++ app/src/main/java/com/platzi/mapsdemo/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.platzi.mapsdemo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+
+private lateinit var map:GoogleMap
+
+class MainActivity : AppCompatActivity(), OnMapReadyCallback {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        createMapFragment()
+    }
+
+    private fun createMapFragment() {
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.fragmentMap) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        map = googleMap
+        createMarker()
+    }
+    private fun createMarker(){
+        val coordinates = LatLng(-12.086741996180411, -77.0037676843122)
+        val marker: MarkerOptions = MarkerOptions().position(coordinates).title("Marcador a personalizar")
+        map.addMarker(marker)
+    }
+}
